9cf49fc51d71343a4f3bd7837da2af4276f75559
GaloDoublier <galodoublier@gmail.com>
2025-02-14 19:41:18 -0300

    HorseSection almost finished - *fix mobile screen

--- src/components/FrameVideo.js
+++ src/components/FrameVideo.js
@@ -3,19 +3,22 @@ import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { horseVideo, frameImg } from "@/utils";
+import Titanium from "./Titanium";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function ZoomOutVideo() {
   const containerRef = useRef(null);
   const videoContainerRef = useRef(null);
-  const frameRef = useRef(null);
+  const doblyRef = useRef(null);
+  const TitleRef = useRef(null);
+  const InfoRef = useRef(null);
 
   useEffect(() => {
     let ctx = gsap.context(() => {
       // Animación para el contenedor del video
       gsap.to(videoContainerRef.current, {
-        scale: 0.7,
+        scale: 0.6,
         ease: "power1.out",
         scrollTrigger: {
           trigger: containerRef.current,
@@ -26,6 +29,39 @@ export default function ZoomOutVideo() {
         },
       });
 
+      gsap.to(doblyRef.current, {
+        opacity:0,
+        ease: "power1.out",
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "top", // Inicio más predecible
+          scrub: 1,
+        },
+      });
+
+      gsap.to(TitleRef.current, {
+        y:-200,
+        opacity:0,
+        ease: "power1.out",
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "top", // Inicio más predecible
+          scrub: 1,
+        },
+      });
+
+      gsap.to(InfoRef.current, {
+        y:0,
+        opacity:1,
+        ease: "power1.out",
+        scrollTrigger: {
+          trigger: InfoRef.current ,
+          start: "top", // Inicio más predecible
+          scrub: 1,
+        },
+      });
+
+
     });
 
     return () => ctx.revert();
@@ -33,29 +69,76 @@ export default function ZoomOutVideo() {
 
   return (
     <>
-      <section ref={containerRef} className="h-[100vh] flex items-center justify-center">
-        <div className="relative w-full h-screen flex items-center justify-center">
+      <section
+        ref={containerRef}
+        className="h-[100vh] flex  items-center justify-center"
+      >
+        <div className="relative w-full h-screen flex flex-col items-center justify-center ">
           {/* Contenedor del video con transformaciones */}
-          <div 
-            ref={videoContainerRef} 
-            className="relative  origin-center transform scale-[1.5]"
+
+          <div
+            ref={videoContainerRef}
+            className="relative  origin-center transform scale-[1.3] z-10 "
           >
-          <img 
-            src={frameImg} 
-            alt="Celular" 
-            className="absolute top-0 left-0 w-[130%] h-[105%] z-10 pointer-events-none"
-          />
-            <video 
-              src={horseVideo} 
-              autoPlay 
-              loop 
-              muted 
-              className="w-full h-full object-cover rounded-[60px]"
+            <div
+              ref={doblyRef}
+              className="bg-black h-full w-full absolute opacity-40"
+            ></div>
+            <h4
+              ref={TitleRef}
+              className="absolute top-1/2 left-1/2 md:text-8xl z-30 text-3xl font-semibold transform -translate-x-1/2 -translate-y-1/2"
+            >
+              4K Dolby Vision a 120 cps.
+              <br></br>
+              Esto es cine.
+            </h4>
+            <img
+              src={frameImg}
+              alt="Celular"
+              className="absolute top-[-5px] md:top-[-10px] w-[110%] left-0 h-[105%] z-10 pointer-events-none"
+            />
+            <video
+              src={horseVideo}
+              autoPlay
+              muted
+              className="w-full h-full object-cover rounded-[10vw]"
             />
           </div>
 
-          {/* Marco del celular - Debe coincidir con el tamaño final del video */}
+          <div ref={InfoRef} className="absolute top-[90%] translate-y-20 opacity-0">
+            <div className="flex md:flex-row flex-col items-start justify-around screen-max-width gap-20 z-0">
+              <div className="flex flex-1  flex-col gap-10">
+              <p className="text-xl w-full font-medium text-gray-100 px-5">
+                El iPhone 16 Pro marca un hito en la grabación de video con
+                nuestra mejor resolución y mayor frecuencia de cuadros hasta
+                ahora: <span className="font-bold text-white">4K Dolby Vision a 120 cps.</span> Gracias a la nueva cámara
+                Fusion de 48 MP con sensor quad pixel de segunda generación y a
+                la potencia del chip A18 Pro, puedes grabar en este increíble
+                formato en modo video o en cámara lenta.
+              </p>
+              <p className="text-xl font-medium text-gray-100 px-5">
+                Ahora, puedes <span className="font-bold text-white">ajustar la velocidad de reproducción después de
+                grabar</span> en la app Fotos, que fue rediseñada para darte más
+                posibilidades al editar. Si quieres darles un efecto de ensueño
+                a tus tomas, prueba la nueva opción a media velocidad. Y si
+                quieres que parezcan verdaderas películas, baja la velocidad a
+                24 cps.
+              </p>
+              </div>
 
+              <div className="flex flex-1 flex-col gap-20">
+              <h5 className="text-3xl w-[65%] mx-auto font-medium text-gray-100">
+              <span className="font-bold text-white">La máxima calidad de video</span> vista en un smartphone
+              </h5>
+              <p className="text-xl font-medium w-[65%] mx-auto text-gray-100">
+              El iPhone 16 Pro también <span className="font-bold text-white">da un salto en rendimiento de audio.</span> Con sus cuatro micrófonos con calidad de estudio vas a poder grabar a nivel pro. Todo va a sonar mucho más fiel a la realidad porque reducen el ruido ambiente a su mínima expresión. Con la nueva grabación de Audio Espacial te vas a sumergir de lleno en tus video cuando uses tus AirPods.
+              </p>
+              </div>
+              
+            </div>
+          </div>
+
+          {/* Marco del celular - Debe coincidir con el tamaño final del video */}
         </div>
       </section>
 
--- src/components/Titanium.js
+++ src/components/Titanium.js
@@ -1,42 +1,65 @@
 "use client"
-import React from 'react'
+import React, { useEffect } from 'react'
 import Glow from './Glow'
 import dynamic from 'next/dynamic';
 import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { useGSAP } from '@gsap/react'
+
+gsap.registerPlugin(ScrollTrigger);
 
 const DynamicTitaniumVideo = dynamic(() => import("../components/TitaniumVideo"), {
   ssr: false,
 });
 
-
-
 const Titanium = () => {
-    useGSAP(() => {
-        gsap.to('.opacity-0', { duration: 2, opacity: 1, delay: 1 });
-    }, []);
+  useEffect(() => {
+    gsap.to('.opacity-0', { duration: 2, opacity: 1, delay: 1 });
+    gsap.fromTo('.animation', 
+      { y: 20, opacity: 0}, 
+
+      { 
+        y: 0, 
+        opacity: 1, 
+        duration: 3, 
+        stagger: 0.25,
+        scrollTrigger: {
+          trigger: '.animation',
+          start: 'top 80%'
+        },
+      }
+    );
+  }, []);
+
   return (
     <section className="common-padding w-full h-full">
-        <div className='flex flex-col leading-none'>
-            
-        <div className="screen-max-width">
-            <div className="flex flex-col items-center relative text-[#c8c2bd] md:text-8xl text-5xl lg:mb-10 mb-16 font-semibold leading-none opacity-0">
-                <p className="text-center">Resistente. Imponente.</p>
-                <p className="text-white" style={{ filter: "url(#glow-1)" }}>
-                    Titanio.
-                </p>
-                <Glow />
-                {/* componente glow sacado de apple.com */}
-            </div>
+      <div className='flex flex-col leading-none'>
+        <div className="screen-max-width py-12">
+          <div className="titanium flex flex-col items-center relative text-[#c8c2bd] md:text-8xl text-5xl lg:mb-10 mb-16 font-semibold leading-none opacity-0">
+            <p className="text-center">Resistente. Imponente.</p>
+            <p className="text-white" style={{ filter: "url(#glow-1)" }}>
+              Titanio.
+            </p>
+            <Glow />
+            {/* componente glow sacado de apple.com */}
+          </div>
         </div>
 
-        <div className="video-container w-[80vw]  relative top-0 left-0 pointer-events-none mx-auto">
-            <DynamicTitaniumVideo />
+        <div className="video-container w-[80vw] relative top-0 left-0 pointer-events-none mx-auto">
+          <DynamicTitaniumVideo />
         </div>
 
-    </div>
+        <div className='text-section h-[40vh] flex xl:flex-row flex-col items-center justify-center gap-10 max-sm:mt-40'>
+          <p className="text-xl animation opacity-0 translate-y-[-20px] md:text-xl xl:w-[25%] font-medium text-gray-100 px-25">
+            El iPhone 16 Pro tiene un sofisticado diseño de titanio grado 5 con una nueva textura microarenada. Como el titanio es uno de los metales con mayor relación peso/dureza, este modelo es <span className="font-bold text-white">increíblemente resistente y liviano.</span> Además, viene en cuatro acabados que se robarán las miradas, como el nuevo titanio del desierto.
+          </p>
+          <p className="text-xl animation opacity-0 translate-y-[-20px] md:text-xl xl:w-[25%] font-medium text-gray-100 px-25">
+            Las novedades no se quedan sólo en el exterior. Con su subestructura térmica 100% reciclada y su vidrio posterior optimizado para disipar mejor el calor, el iPhone 16 Pro tiene un <span className="font-bold text-white">rendimiento continuo</span> hasta un 20% superior al del iPhone 15 Pro. Prepárate para hacer todo lo que te gusta y jugar al más alto nivel por horas y horas.
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
 
-export default Titanium
+export default Titanium;
